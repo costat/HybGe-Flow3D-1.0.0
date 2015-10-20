5dfe5b83ff830185c5aac2adfa297fd07d46fa3e
Timothy <costa.timothy@gmail.com>
2015-10-19 22:19:38 -0700

    Link openmp in test compile script.

--- test.bash
+++ test.bash
@@ -1,1 +1,1 @@
-g++ runtest.cpp hgf.cpp hgfMesh.cpp hgfArrays.cpp hgfBC.cpp hgfIB.cpp hgfPP.cpp -pg -O3 -o hgfTest -lparalution
+g++ runtest.cpp hgf.cpp hgfMesh.cpp hgfArrays.cpp hgfBC.cpp hgfIB.cpp hgfPP.cpp -pg -O3 -o hgfTest -lparalution -fopenmp
