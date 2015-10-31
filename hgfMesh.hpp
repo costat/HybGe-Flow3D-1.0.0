a6bf57fda781b22cefa21a7f4607034bcddeec68
Timothy Costa <costa.timothy@gmail.com>
2015-10-30 20:35:25 -0700

    private functions

--- hgfMesh.hpp
+++ hgfMesh.hpp
@@ -32,6 +32,7 @@ class FluidMesh
     void BuildUniformMesh( unsigned long *gridin, int ldi1, int ldi2, \
                     int nx, int ny, int nz, \
                     double length, double width, double height );
+  private:
     int isNear2d( std::vector<double>& Vector1, std::vector<double>& Vector2, \
                   double dx, double dy, double dz, int nNodes );
     int isNear3d( std::vector<double>& Vector1, std::vector<double>& Vector2, \
