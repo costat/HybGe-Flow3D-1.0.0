81253261dc94f319a34dc36d7b6126117b4261b3
Timothy <costa.timothy@gmail.com>
2016-02-22 20:07:25 -0800

    removed boost links / finds. keeping simple build for omp/paralution version.

--- OMP/hgfSetup.py
+++ OMP/hgfSetup.py
@@ -13,16 +13,16 @@ except AttributeError:
 
 hgf_module = Extension( '_hgf',
                        sources=['hgf.i', 'hgf.cpp', 'hgfMesh.cpp', 'hgfStokes.cpp', 'hgfPoreNetwork.cpp', 'hgfArrays.cpp', 'hgfBC.cpp', 'hgfIB.cpp', 'hgfPP.cpp'],
-		       include_dirs = [numpy_include],
+		               include_dirs = [numpy_include],
                        extra_compile_args=['-O3','-fopenmp'],
                        extra_link_args=['-lparalution'],
-		       swig_opts=['-c++']
-		      )
+		               swig_opts=['-c++']
+		              )
 
 setup ( name = 'HybGe-Flow3d',
         version = '0.0.1',
         author = 'Timothy B. Costa',
-	description = '2D/3D CFD software with Immersed Boundary',
-	ext_modules = [hgf_module],
-	py_modules = ["hgf"]
+	    description = '2D/3D CFD software with Immersed Boundary',
+	    ext_modules = [hgf_module],
+	    py_modules = ["hgf"]
       )
