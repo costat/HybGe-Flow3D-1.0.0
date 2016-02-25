ccc3da0eb865a63d37a51c62133c7486044f526f
Timothy <costa.timothy@gmail.com>
2016-02-25 14:08:15 -0800

    removed rich solvers.

--- src/hgfStokesM.hpp
+++ src/hgfStokesM.hpp
@@ -9,18 +9,5 @@
 
 void
 StokesSolveDirect( const FluidMesh& Mesh, std::vector<double>& Solution, const ProbParam& Par );
-void
-StokesSolveRich( const FluidMesh& Mesh, double visc, int direction, \
-                 std::vector<double>& Solution, double tolAbs, double tolRel, \
-                 int maxIt, int nThreads, int prec, double relax );
-void
-initPressure( const FluidMesh& Mesh, std::vector<double>& Solution, int direction );
-void
-setForceRich( const FluidMesh& Mesh, const std::vector<double>& Solution, std::vector<double>& b, \
-              const std::vector<double>& force, int component );
-void
-updatePressureRich( const FluidMesh& Mesh, std::vector<double>& Solution, \
-                    const std::vector<double>& solU, const std::vector<double>& solV, \
-                    const std::vector<double>& solW, double& residual, double relax );
 
 #endif
