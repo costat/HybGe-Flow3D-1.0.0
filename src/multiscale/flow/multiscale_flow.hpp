3c441ae1db6dad80fdc802ce67186b4a8339d238
Timothy <costa.timothy@gmail.com>
2017-01-06 12:06:39 -0800

    Functions to compute upscaled permeability from porescale flow solutions

--- src/multiscale/flow/multiscale_flow.hpp
+++ src/multiscale/flow/multiscale_flow.hpp
@@ -3,16 +3,24 @@
 
 #include "../../../include/hgflow.hpp"
 
-// system includes
-#include <vector>
-
 namespace hgf
 {
   namespace multiscale
   {
     namespace flow
     {
-
+      double compute_permeability_x(const parameters& par, const std::vector< degree_of_freedom >& velocity_u, \
+                                                           const std::vector< degree_of_freedom >& velocity_v, \
+                                                           const std::vector< degree_of_freedom >& velocity_w, \
+                                                           const std::vector< double > solution);
+      double compute_permeability_y(const parameters& par, const std::vector< degree_of_freedom >& velocity_u, \
+                                                           const std::vector< degree_of_freedom >& velocity_v, \
+                                                           const std::vector< degree_of_freedom >& velocity_w, \
+                                                           const std::vector< double > solution);
+      double compute_permeability_z(const parameters& par, const std::vector< degree_of_freedom >& velocity_u, \
+                                                           const std::vector< degree_of_freedom >& velocity_v, \
+                                                           const std::vector< degree_of_freedom >& velocity_w, \
+                                                           const std::vector< double > solution);
     }
   }
 }
